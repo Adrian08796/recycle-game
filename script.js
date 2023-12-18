48f71ccee53e336be783d8b8ca230aee6e9c1400
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2023-12-18 18:28:03 +0100

    second commit

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function loadRandomTrashPhoto() {
       .catch(error => console.error('Error fetching random trash photo:', error));
   }
   
+  function drag(ev) {
+    ev.dataTransfer.setData('text', ev.target.id);
+  }
+  
+  function allowDrop(ev) {
+    ev.preventDefault();
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData('text');
+    const draggedElement = document.getElementById(data);
+    const dropTarget = ev.target;
+  
+    // Append the dragged image to the drop container
+    if (dropTarget.classList.contains('drop-container')) {
+      dropTarget.appendChild(draggedElement);
+    } else if (dropTarget.classList.contains('image-container')) {
+      // If dropping on the image container, move it back to its original position
+      const originalContainer = document.getElementById('image-container');
+      originalContainer.appendChild(draggedElement);
+    }
+  }
+  
